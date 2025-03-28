9e59cf1d391bf17ab98d7f36a5c614292296f844
Maliha Rahman Moon <malihamoon05@gmail.com>
2025-03-28 22:51:56 +0600

    Add files via upload

---
+++ src/project/responsive.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Responsive = () => {
+  return (
+    <div>
+      <p className="text-xs px-1 bg-amber-700 mx-4 text-white sm:text-sm md:text-lg lg:text-xl xl:text-2xl sm:bg-blue-500 md:bg-cyan-600 lg:bg-emerald-600 xl:bg-fuchsia-400 2xl:bg-pink-600">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam voluptate
+        repellendus vitae reiciendis iste rem rerum, enim animi quidem ea amet?
+        Voluptas nihil esse corporis excepturi deleniti, magni totam animi?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam voluptate
+        repellendus vitae reiciendis iste rem rerum, enim animi quidem ea amet?
+        Voluptas nihil esse corporis excepturi deleniti, magni totam animi?
+      </p>
+    </div>
+  );
+};
+
+export default Responsive;
